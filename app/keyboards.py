02d4f9ba386f0998ffbbd3e80d66c16267a81359
kirillLeo1 <128916837+kirillLeo1@users.noreply.github.com>
2025-10-24 18:37:22 +0200

    Delete app directory

--- app/keyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from .i18n import I18N
-
-def main_menu(lang: str) -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text=I18N["btn_tasks"][lang])],
-        [KeyboardButton(text=I18N["btn_profile"][lang])],
-        [KeyboardButton(text=I18N["btn_withdraw"][lang])],
-    ], resize_keyboard=True)
